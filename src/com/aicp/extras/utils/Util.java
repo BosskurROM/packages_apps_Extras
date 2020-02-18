8f47ac5e7832f741b9aecf9497c3cb95b0252f22
xlm03lx <55821303+xlm03lx@users.noreply.github.com>
2020-02-18 19:22:37 +0800

    todak : update path 2/3

--- src/com/aicp/extras/utils/Util.java
+++ src/com/aicp/extras/utils/Util.java
@@ -113,7 +113,7 @@ public abstract class Util {
    }
 
    public static String getDownloadLinkForDevice(Context context) {
-        return "https://floko.maud.io";
+        return "https://todak.com";
    }
 
    public static String readStringFromFile(File inputFile) throws IOException {
